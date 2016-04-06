7a6ae141aca20fc2b200956b751badbf079e8099
Mike Berkman <mike@berkman.co>
2016-04-05 20:25:42 -0400

    more testing

--- Foosball-Test.py
+++ Foosball-Test.py
@@ -3,39 +3,43 @@
 from Foosball import FoosballMatch, FoosballTeam, FoosballPlayer
 from random import randint
 
-my_match = FoosballMatch(2, 5,
-    FoosballTeam("Ohio", FoosballPlayer("Drew"), FoosballPlayer("Mike")),
-    FoosballTeam("SLC", FoosballPlayer("Eric"), FoosballPlayer("Jedidiah"))
-    )
+players = [
+    FoosballPlayer("Drew"),
+    FoosballPlayer("Mike"),
+    FoosballPlayer("Eric"),
+    FoosballPlayer("Jedidiah"),
+    FoosballPlayer("Alex"),
+    FoosballPlayer("Christian")
+    ]
 
-print my_match.team1.display_team()
-print my_match.team2.display_team()
-#for i in range(20):
-while my_match.winner is False:
-    print my_match.get_score()
+## TODO: randomize the teams/pairings?  save to file?
 
-    scoring_team = randint(1,2)
+for i in range(5):
+    my_match = FoosballMatch(
+        2,
+        5,
+        FoosballTeam("Ohio", players[0], players[1]),
+        FoosballTeam("SLC", players[2], players[3])
+        )
 
-    if scoring_team == 1:
-        my_match.score_goal(my_match.team1)
-    elif scoring_team == 2:
-        my_match.score_goal(my_match.team2)
+    print my_match.team1.display_team()
+    print my_match.team2.display_team()
 
-print my_match.team1.player1.get_wins()
-print my_match.team1.player2.get_wins()
-print my_match.team2.player1.get_wins()
-print my_match.team2.player2.get_wins()
+    while my_match.winner is False:
+        print my_match.get_score()
+
+        scoring_team = randint(1,2)
+
+        if scoring_team == 1:
+            my_match.score_goal(my_match.team1)
+        elif scoring_team == 2:
+            my_match.score_goal(my_match.team2)
+
+    for player in players:
+        print player.get_wins()
+
 
 '''
-pseudo code
-
-create a match
-
-loop
-    has a team won the match? -- how do we determine this?
-        yes
-            break
-        no
-            pick a random team number
-            that team scores a goal
+-keep statistics not only for individual wins, but wins by pairing/team
+-winners stay at table
 '''
